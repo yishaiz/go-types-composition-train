12e3fb01b268f5800dcebeef8d5a0bdaa0ded3ee
yishai <yishai5555@gmail.com>
2023-06-11 18:40:33 +0300

    07-function-naked-return

--- main.go
+++ main.go
@@ -7,34 +7,11 @@ import "fmt"
 // interface type
 
 func main() {
-	// var myMap map[string]string
+	z := addTwoNumbers(2, 4)
+	fmt.Println(z)
+}
 
-	intMap := make(map[string]int)
-
-	intMap["one"] = 1
-	intMap["two"] = 2
-	intMap["three"] = 3
-	intMap["four"] = 4
-	intMap["five"] = 5
-
-	for key, value := range intMap {
-		fmt.Println(key, value)
-	}
-
-	fmt.Println("*** delete four *** ")
-	delete(intMap, "four")
-
-	for key, value := range intMap {
-		fmt.Println(key, value)
-	}
-
-	// check if exist
-
-	el, ok := intMap["four"]
-	if ok {
-		fmt.Println(el, "is in map")
-	} else {
-		fmt.Println(el, "is NOT in map")
-	}
-
+func addTwoNumbers(x, y int) (sum int) {
+	sum = x + y
+	return
 }
---
+++ map.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// reference types (pointers, slices, maps, functions, channels)
+
+// interface type
+
+func map() {
+	// var myMap map[string]string
+
+	intMap := make(map[string]int)
+
+	intMap["one"] = 1
+	intMap["two"] = 2
+	intMap["three"] = 3
+	intMap["four"] = 4
+	intMap["five"] = 5
+
+	for key, value := range intMap {
+		fmt.Println(key, value)
+	}
+
+	fmt.Println("*** delete four *** ")
+	delete(intMap, "four")
+
+	for key, value := range intMap {
+		fmt.Println(key, value)
+	}
+
+	// check if exist
+
+	el, ok := intMap["four"]
+	if ok {
+		fmt.Println(el, "is in map")
+	} else {
+		fmt.Println(el, "is NOT in map")
+	}
+
+}
